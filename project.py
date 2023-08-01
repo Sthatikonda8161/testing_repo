cc616d05e01af5187da36ff0bc61b604fe188035
Sthatikonda8161 <kiransai.8161@gmail.com>
2023-08-02 04:01:11 +0530

    code added

--- project.py
+++ project.py
@@ -1,1 +1,19 @@
-import maths
+# login_page.py
+import tkinter as tk
+
+class LoginPage(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Login Page")
+        self.geometry("300x150")
+
+        self.username_label = tk.Label(self, text="Username:")
+        self.username_label.pack()
+        self.username_entry = tk.Entry(self)
+        self.username_entry.pack()
+
+    
+
+if __name__ == "__main__":
+    app = LoginPage()
+    app.mainloop()
